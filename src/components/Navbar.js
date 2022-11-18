a954da57d8f37114e810575f7e8617a3205ec7ce
Diego <diegopena2022@gmail.com>
2022-11-17 18:14:15 -0800

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css"
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location])
+
+    return (
+        <div className="navbar" id={expandNavbar ? "open" : "close"}>
+            <div className="toggleButton">
+                <button onClick={() => {
+                    setExpandNavbar((prev) => !prev);
+                }}>
+                    <ReorderIcon />
+                </button>
+            </div>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/Projects">Projects</Link>
+                <Link to="/Contact">Contact</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
